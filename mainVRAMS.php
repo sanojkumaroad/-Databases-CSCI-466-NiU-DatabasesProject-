ecfcb59a4f674f37c7a37664e39a30d1f7c5b0ea
vscortez <143911596+vscortez@users.noreply.github.com>
2023-11-29 16:42:55 -0600

    Add files via upload

---
+++ mainVRAMS.php
@@ -0,0 +1,16 @@
+<!-- VRAMS MAIN PAGE -->
+
+
+<!DOCTYPE HTML>
+<HTML>
+
+<head>
+<title> VRAMS </title>
+</head>
+
+<img src = "VRAMS Logo.png" alt = "VRAMS logo" width = "200" height = "150">
+
+<h1 style = "font - family:Bernard MT"> Selling Quality Shoes to All <h1>
+<img src = "MainPicture.png" alt = "shoe styles" width = 100% height = "300">
+</HTML>
+
