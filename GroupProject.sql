d31e8bd3f80e270c58a1eaf140875df8afe2c5ad
vscortez <143911596+vscortez@users.noreply.github.com>
2023-11-28 19:05:15 -0600

    Update GroupProject.sql

--- GroupProject.sql
+++ GroupProject.sql
@@ -1,18 +1,21 @@
---Vanessa Cortez-Ocon
+--VRAMS
 --Section2
 --GROUP PROJECT 
 
 
+-- SHOW CREATE DATABASE VRAMS;
+
 --entities tables
 CREATE TABLE User_Customer(CustomerID INT AUTO_INCREMENT PRIMARY KEY, Address VARCHAR(300), Password VARCHAR(35), Email VARCHAR(75), Phone_num VARCHAR(15), FirstName VARCHAR(50), LastName VARCHAR(50));
-
 CREATE TABLE User_Employee(EmployeeID INT AUTO_INCREMENT PRIMARY KEY, Address VARCHAR(300), Password VARCHAR(10), Email VARCHAR(75), Phone_num INT(11), FirstName VARCHAR(50), LastName VARCHAR(50));
 
 CREATE TABLE Products(ProductID INT AUTO_INCREMENT PRIMARY KEY, product_name VARCHAR(50), product_description VARCHAR(200), quantity INT, product_price FLOAT);
 
+--         NOT WORKING
 CREATE TABLE Order_Info(OrderID INT AUTO_INCREMENT PRIMARY KEY, CustomerID INT, ProductID INT, OrderDate VARCHAR(10), Status VARCHAR(20), TotalAmount FLOAT, FOREIGN KEY(CustomerID) REFERENCES User_Customer(CustomerID), FOREIGN KEY(ProductID) REFERENCES Products(ProductID));
 
 --CREATE TABLE Order_Info(OrderID INT AUTO_INCREMENT, CustomerID INT, ProductID INT, OrderDate VARCHAR(10), Status VARCHAR(20), TotalAmount FLOAT, PRIMARY KEY (OrderID), FOREIGN KEY(CustomerID) REFERENCES UserCustomer(CustomerID), FOREIGN KEY(ProductID) REFERENCES product(ProductID));
+
 
 
 CREATE TABLE Promo_Code(PromocodeID INT AUTO_INCREMENT PRIMARY KEY, discountpercent INT, ExpiryDate VARCHAR(10));
@@ -26,6 +29,8 @@ CREATE TABLE Order_Tracking(OrderID INT,  OrderStatID INT, ConfirmNumber INT NOT
 CREATE TABLE Customer_Billing(OrderID INT PRIMARY KEY, CustomerID INT, CardName VARCHAR(50), CardNumber INT NOT NULL, CardExpiration VARCHAR(5), CVV VARCHAR(3), BillingAddress VARCHAR(300), FOREIGN KEY(CustomerID) REFERENCES User_Customer(CustomerID), FOREIGN KEY(OrderID) REFERENCES Order_Info(OrderID));
 
 CREATE TABLE Order_Discount(OrderID INT, PromocodeID INT, PRIMARY KEY (OrderID, PromocodeID), FOREIGN KEY(OrderID) REFERENCES Order_Info(OrderID), FOREIGN KEY(PromocodeID) REFERENCES Promo_Code(PromocodeID));
+
+
 
 -- Inserting 5 Customers
 
