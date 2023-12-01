0f0931a04c606800513ded697cde8f6c2e9328b3
Rumel Piolo Dangcalan <143548138+z1908204@users.noreply.github.com>
2023-12-01 11:06:08 -0600

    Update InventoryCheck.php

--- InventoryCheck.php
+++ InventoryCheck.php
@@ -1,13 +1,13 @@
 <?php
 
 //Opening a connection with our database and creating queries needed
-$connection = mysqli_connect('courses', 'z-number', 'password');
+$connection = mysqli_connect('courses', 'z1908204', '2001Nov05');
 if(!$connection)
 {
   die("Connection error");
 }
 
-$database = mysqli_select_db($connection,"z-number");
+$database = mysqli_select_db($connection,"z1908204");
 
 if(!$database)
 {
@@ -19,47 +19,133 @@ $queryID = mysqli_query($connection, "select ProductID from Products");
 
 ?>
 
-
 <!DOCTYPE html>
-<html>
 
-    <!--            Used to split page in half and costimize color          -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory. VRAMS Shoe Store.</title>
+    <link rel="stylesheet" href="styles.css">
+    
+<!--         "I DELETED THIS PART BUT INCLUDED OUR HEADER SECTION INSTEAD! SO DELETE THIS MESSAGE ONCE YOU UNDERSTAND WHAT I DID HERE!!!   Used to split page in half and costimize color          -->
 <style>
-.split {
-  height: 100%;
-  width: 50%;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  overflow-x: hidden;
-  padding-top: 20px;
-}
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #FFB6C1;
+        }
 
-.left {
-  left: 0;
-  background-color: #FFB6C1;
-}
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1em 0;
+        }
 
-.right {
-  right: 0;
-  background-color: #FFC0CB;
-}
+        header a {
+            color: #fff;
+            text-decoration: none;
+            margin: 0 15px;
+	}
 
-}
+	        .navbar {
+            background-color: #333;
+            overflow: hidden;
+        }
+
+        .navbar a {
+            float: left;
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        .navbar a:hover {
+            background-color: #ddd;
+            color: black;
+        }
+
+        .navbar .icon {
+            display: none;
+        }
+
+        @media screen and (max-width: 600px) {
+            .navbar a:not(:first-child) {display: none;}
+            .navbar a.icon {
+                float: right;
+                display: block;
+            }
+        }
+
+        @media screen and (max-width: 600px) {
+            .navbar.responsive {position: relative;}
+            .navbar.responsive .icon {
+                position: absolute;
+                right: 0;
+                top: 0;
+            }
+            .navbar.responsive a {
+                float: none;
+                display: block;
+                text-align: left;
+            }
+	}
+
+        main {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+            padding: 20px;
+        }
+
+        .contact-us {
+            background-color: #333;
+            border: 1px solid #fff;
+            color: #fff;
+            padding: 20px;
+            margin: 10px;
+            text-align: center;
+        }
+
+        footer {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1em 0;
+        }
+
+        footer a {
+            color: #fff;
+            text-decoration: none;
+            margin: 0 15px;
+        }
 
 </style>
 
 
-<head>
-
-<title> VRAMS - Inventory Section </title>
-
-</head>
-
     <!--        Setting left to show list of all products        -->
 
 <body>
-<div class = "split left">
+
+    <!--	HEADER SECTION THAT I INCLUDED!		-->
+
+    <header>
+    <div class="navbar" id="navbar">
+      	    <a href="mainVRAMS.php"><img src = "VRAMS Logo.png" alt = "VRAMS logo" width = "200" height = "150"></a
+>           <a href="shop.php">Shop</a>  
+	    <a href="InventoryCheck.php">Inventory</a>
+            <a href="AboutUs.php">About Us</a>
+            <a href="Contact.php">Contact</a>
+            <a href="Login.php">Login/Sign Up</a>
+            <a href="cart.php">Cart</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()"> &#9776; </a>
+    </div>
+</header>
+
 
 <h1> Full Product List </h1>
 <table border="2" style = "background-color: white;">
@@ -117,7 +203,7 @@ $queryID = mysqli_query($connection, "select ProductID from Products");
         <th> Quantity </th>
         </tr>
         <?php
-        while($row = mysqli_fetch_array($queryINV,MYSQLI_ASSOC))
+        while($row = mysqli_fetch_array($queryINV, MYSQLI_ASSOC))
         {?>
             <tr>
             <td> <?php echo $row['ProductID']; ?> </td>
@@ -184,4 +270,3 @@ $queryID = mysqli_query($connection, "select ProductID from Products");
 
 </body>
 </html>
-
