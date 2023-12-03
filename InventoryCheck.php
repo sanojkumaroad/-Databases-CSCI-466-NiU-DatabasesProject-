0b0b76b8bad815af5d0b7f4eb9b5f7950599f56f
Sanoj Kumar Oad <sanojkumaroad@gmail.com>
2023-12-03 10:25:22 -0600

    Update InventoryCheck.php

--- InventoryCheck.php
+++ InventoryCheck.php
@@ -15,7 +15,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Inventory | VRAMS Shoe Store</title>
-        <link rel="stylesheet" href="stylesInventoryCheck.css">   
+        <link rel="stylesheet" href="css/stylesInventoryCheck.css">   
     </head>
     
     <body>
