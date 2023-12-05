74e865ed4e205a68e668922bd3e150199d7535df
Rumel Piolo Dangcalan <143548138+z1908204@users.noreply.github.com>
2023-12-05 17:32:28 -0600

    Update orderFulfillment.php

--- orderFulfillment.php
+++ orderFulfillment.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Order Fulfillment</title>
-    <link rel="stylesheet" href="css/stylesOrderFulfillment.css">
+    <title>VRAMS Shoe Order Tracking</title>
+    <link rel="stylesheet" href="stylesOrderTracking.css">
 </head>
 <body>
 
-    <h2>Order Fulfillment</h2>
+    <h2>Order Tracking for Customer</h2>
 
     <?php
         include 'connection.php';
@@ -17,63 +17,41 @@
 
         // Check if the form is submitted
         if (isset($_POST['submit'])) {
-            // Get the order ID and fulfillment details from the form
-            $orderID = $_POST['order_id'];
-            $trackingStatus = $_POST['tracking_status'];
-            $notes = $_POST['notes'];
+            // Get the tracking ID from the form
+            $trackingID = $_POST['tracking_id'];
 
-            // Update the Order_Tracking table with fulfillment details
-            $updateStmt = $connection->prepare("
-                UPDATE Order_Tracking
-                SET TrackingStatus = :tracking_status, TrackingStatus = :notes, LastUpdateDate = CURRENT_TIMESTAMP
-                WHERE OrderID = :order_id
+            // Prepare and execute the SQL query
+            $stmt = $connection->prepare("
+                SELECT Order_Tracking.TrackingID, Order_Tracking.TrackingStatus, Orders.OrderID, Orders.OrderDate, Orders.OrderTotal, Orders.OrderStatus
+                FROM Order_Tracking
+                JOIN Orders ON Order_Tracking.OrderID = Orders.OrderID
+                WHERE Order_Tracking.TrackingID = :tracking_id
             ");
-            $updateStmt->bindParam(':order_id', $orderID);
-            $updateStmt->bindParam(':tracking_status', $trackingStatus);
-            $updateStmt->execute();
+            $stmt->bindParam(':tracking_id', $trackingID);
+            $stmt->execute();
 
-            echo "<p>Order ID " . $orderID . " has been updated.</p>";
-        }
+            // Fetch the result
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        // Fetch orders from the Order_Tracking table
-        $stmt = $connection->query("
-            SELECT Order_Tracking.OrderID, Orders.CustomerID, Orders.OrderDate, Orders.OrderTotal, Order_Tracking.TrackingStatus
-            FROM Order_Tracking
-            JOIN Orders ON Order_Tracking.OrderID = Orders.OrderID
-        ");
-        $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        if ($orders) {
-            echo "<table>";
-            echo "<tr><th>Order ID</th><th>Customer ID</th><th>Order Date</th><th>Order Total</th><th>Tracking Status</th><th>Action</th></tr>";
-
-            foreach ($orders as $order) {
-                echo "<tr>";
-                echo "<td>" . $order['OrderID'] . "</td>";
-                echo "<td>" . $order['CustomerID'] . "</td>";
-                echo "<td>" . $order['OrderDate'] . "</td>";
-                echo "<td>$" . $order['OrderTotal'] . "</td>";
-                echo "<td>" . $order['TrackingStatus'] . "</td>";
-                echo "<td>
-                        <form method='post'>
-                            <input type='hidden' name='order_id' value='" . $order['OrderID'] . "'>
-                            <label for='tracking_status'>Tracking Status:</label>
-                            <input type='text' name='tracking_status' required>
-                            <br>
-                            <label for='notes'>Notes:</label>
-                            <textarea name='notes'></textarea>
-                            <br>
-                            <button type='submit' name='submit'>Update Order</button>
-                        </form>
-                      </td>";
-                echo "</tr>";
+            // Check if the tracking ID is found
+            if ($result) {
+                echo "<p>Order Status: " . $result['OrderStatus'] . "</p>";
+                echo "<p>Tracking Status: " . $result['TrackingStatus'] . "</p>";
+                echo "<p>Order Date: " . $result['OrderDate'] . "</p>";
+                echo "<p>Order Total: $" . $result['OrderTotal'] . "</p>";
+            } else {
+                echo "<p>Invalid tracking ID. Please check your tracking ID and try again.</p>";
             }
-
-            echo "</table>";
-        } else {
-            echo "<p>No orders found for fulfillment.</p>";
         }
     ?>
+
+    <!-- Tracking ID input form -->
+    <form method="post">
+        <label for="tracking_id">Enter Tracking ID:</label>
+        <input type="text" name="tracking_id" required>
+        <br>
+        <button type="submit" name="submit">Track Order</button>
+    </form>
 
 </body>
 </html>
