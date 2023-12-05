984ebb7f2d750db452399e0559fab070f6ffb17c
Rumel Piolo Dangcalan <143548138+z1908204@users.noreply.github.com>
2023-12-05 17:26:27 -0600

    Update AboutUs.php

--- AboutUs.php
+++ AboutUs.php
@@ -8,20 +8,20 @@
     </head>
     
     <body>
-        <header>
-            <div class="navbar" id="navbar">
-      	        <a href="mainVRAMS.php"><img src = "VRAMSLogo.png" alt = "VRAMS logo" width = "200" height = "150"></a>
-                <a href="shop.php">Shop</a>  
-	        <a href="InventoryCheck.php">Employee</a>
-                <a href="AboutUs.php">About Us</a>
-		<a href="Contact.php">Contact</a>
-	        <a href="Login.php">Login/Sign Up</a>
-		<a href="cart.php">Cart</a>
-                <a href="javascript:void(0);" class="icon" onclick="myFunction()"> &#9776; </a>
-            </div>
-        </header>
-
-        <main>
+            <header>
+        <div class="navbar" id="navbar">
+            <a href="mainVRAMS.php"><img src="VRAMSLogo.png" alt="VRAMS logo" width="200" height="150"></a>
+            <a href="shop.php">Shop</a>
+            <a href="orderTracking.php">Track Order</a>
+            <a href="AboutUs.php">About Us</a>
+            <a href="Contact.php">Contact</a>
+            <a href="Login.php">Login/Sign Up</a>
+            <a href="InventoryCheck.php">Employee</a>
+            <a href="cart.php">Cart</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()"> &#9776; </a>
+        </div>
+    </header>
+	<main>
             <div class="founders-section">
                 <h2>Meet Our Founders</h2>
                 
