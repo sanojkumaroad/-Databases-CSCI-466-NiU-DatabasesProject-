024a0934071db227c71975dcb16bf8faa2d6cc29
Rumel Piolo Dangcalan <143548138+z1908204@users.noreply.github.com>
2023-12-05 17:33:03 -0600

    Update outstandingOrders.php

--- outstandingOrders.php
+++ outstandingOrders.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Outstanding Orders</title>
-    <link rel="stylesheet" href="css/stylesOutstandingOrders.css">
+    <link rel="stylesheet" href="stylesOutstandingOrders.css">
 </head>
 <body>
 
