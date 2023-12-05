8fbca5b22ab16e0726207fdbec75b0b42a3737bf
Sanoj Kumar Oad <sanojkumaroad@gmail.com>
2023-12-04 20:04:25 -0600

    Update Login.php
    Reverted the changes.
    Was supposed to change Employee_Verify.php instead.

--- Login.php
+++ Login.php
@@ -1,4 +1,12 @@
 <!-- Login.php -->
+
+<?php
+    session_start();
+    include 'connection.php';
+    // $connection is available and connected to the database.
+    // Perform your database operations using $connection.
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -7,7 +15,6 @@
         <title>Login | VRAMS Shoe Store</title>
         <link rel="stylesheet" href="css/stylesLogin.css">
     </head>
-
     <body>
         <header>
             <div class="navbar" id="navbar">
@@ -21,69 +28,76 @@
                 <a href="javascript:void(0);" class="icon" onclick="myFunction()"> &#9776; </a>
             </div>
         </header>
-        
+
         <main>
             <h2>Login</h2>
-	    
-        <?php
-		    include "connection.php";
-		    // $connection is available and connected to the database.
-		    // Perform your database operations using $connection.
 
-    		// Check if the form is submitted
-    		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            <?php
+                if (isset($loginError)) {
+                    echo '<p style="color: red;">' . $loginError . '</p>';
+                }  elseif (isset($_SESSION['user_logged_in'])) {
+                    echo '<p style="color: green;">You have successfully logged in as ' . $_SESSION['user_email'] . '.</p>';
+                    // Redirect to home page
+                    header('refresh:3; url=mainVRAMS.php'); // Redirect after 3 seconds
+                    exit();
+                }
+            ?>
 
-        	    // Get user input
-        	    $email = isset($_POST['email']) ? $_POST['email'] : '';
-        	    $password = isset($_POST['password']) ? $_POST['password'] : '';
-
-        	    // Query to check if the email and password match
-        	    $sql = "SELECT * FROM User_Employee WHERE email='$email' AND password='$password'";
-        
-        	    // Execute the query and check for errors
-        	    $result = $connection->query($sql);
-        	    if (!$result) {
-			        die("Error in SQL query: " . $connection->error);
-        	    }
-
-        	    // Check if there is a matching user
-        	    $num_rows = $result->num_rows;
-        	    
-		        if ($num_rows > 0) {
-            		// Redirect to another page on successful login
-            		header("Location: InventoryCheck.php");
-            		exit();
-        	    } else {
-            		// Login Failed!
-			        echo '<p style="color: red;">' . "Invalid Email or Password. Please Try Again!" . '</p>';
-        	    }
-
-        	    // Close the database connection
-        	    $connection->close();
-		    }
-	    ?>
-    
-	        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+            <form method="post" action="login.php" onsubmit="return validateForm('login')">
                 <label for="login-email">Email:</label>
                 <input type="email" id="login-email" name="email" required> <br>
                 <label for="login-password">Password:</label>
                 <input type="password" id="login-password" name="password" required> <br>
                 <button type="submit" name="login">Login</button>
             </form>
-            
+
+            <h2>Create Account</h2>
+
+            <?php
+                if (isset($createAccountError)) {
+                    echo '<p style="color: red;">' . $createAccountError . '</p>';
+                } elseif (isset($_SESSION['user_logged_in'])) {
+                    echo '<p style="color: green;">Account created and logged in as ' . $_SESSION['user_email'] . '.</p>';
+                    // Redirect to home page
+                    header('refresh:3; url=mainVRAMS.php'); // Redirect after 3 seconds
+                    exit();
+                }
+            ?>
+
+            <form method="post" action="login.php" onsubmit="return validateForm('create')">
+                <label for="create-email">Email:</label>
+                <input type="email" id="create-email" name="email" required> <br>
+                <label for="create-password">Password:</label>
+                <input type="password" id="create-password" name="password" required oninput="checkPassword('create')">
+
+                <div class="password-status" id="create-password-status"> </div> <br>
+
+                <div id="password-requirements">
+                    <p>Password Requirements:</p>
+                    <ul>
+                        <li id="length-req">Minimum 8 characters</li>
+                        <li id="uppercase-req">At least one uppercase letter</li>
+                        <li id="lowercase-req">At least one lowercase letter</li>
+                        <li id="digit-req">At least one digit</li>
+                        <li id="symbol-req">At least one symbol</li>
+                    </ul>
+                </div> <br>
+
+                <button type="submit" name="create_account" id="create-account-btn" >Create Account</button>
+            </form>
+
             <script>
                 function checkPassword(formType) {
+  
                     const passwordInput = document.getElementById(`${formType}-password`);
                     const passwordStatus = document.getElementById(`${formType}-password-status`);
                     const password = passwordInput.value;
-
                     // Password restrictions
                     const lengthRegex = /.{8,30}/;
                     const uppercaseRegex = /[A-Z]/;
                     const lowercaseRegex = /[a-z]/;
                     const digitRegex = /\d/;
                     const symbolRegex = /[!@#$%^&*(),.?":{}|<>]/;
-
                     // Check if all restrictions are met
                     const isValid = (
                         lengthRegex.test(password) &&
@@ -92,7 +106,6 @@
                         digitRegex.test(password) &&
                         symbolRegex.test(password)
                     );
-
                     // Display appropriate status and update button state
                     passwordStatus.innerHTML = isValid
                     ? '<img src="checkmark.png" alt="Checkmark">'
@@ -100,10 +113,15 @@
 
                     const createAccountBtn = document.getElementById(`${formType}-account-btn`);
                     createAccountBtn.disabled = !isValid;
-		        }
-	        </script>
+                }
+
+                function validateForm(formType) {
+                    // Additional validation logic if needed
+                    return true; // Return true to submit the form
+                }
+            </script>
         </main>
-        
+
         <footer> <p>All rights reserved. © 2023 VRAMS Shoe Store</p> </footer>
     </body>
 </html>
