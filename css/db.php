f1f224b5c454ab5965ba5776e8a28dac85248cd0
Rumel Piolo Dangcalan <143548138+z1908204@users.noreply.github.com>
2023-12-05 18:03:01 -0600

    Create db.php

---
+++ css/db.php
@@ -0,0 +1,15 @@
+<!-- db.php -->
+
+<?php
+$host = "courses";
+$dbname = "z-id";
+$user = "z-id";
+$password = "password";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
